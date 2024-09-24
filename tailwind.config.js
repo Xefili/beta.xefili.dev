5b25c408fb53df2c66592c5d40ebbcbdfddc2f45
xefili <henrycarl2007@gmail.com>
2024-09-24 15:20:21 +0200

    Add TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  content: ['*.html', "en//api//*.html", "de//api//*.html", "fr//api//*.html"],
+  theme: {
+    fontFamily: {
+      'circle': ['Plus Jakarta Sans', 'sans-serif'],
+      'circleBold': ['Plus Jakarta Sans Extrabold', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [
+    autoprefixer
+  ],
+}
+
