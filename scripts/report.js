d6bed2c173623cdd5b2afbdc749cc956fa46794a
xefili <henrycarl2007@gmail.com>
2024-09-30 12:30:55 +0200

    experimental report system

---
+++ scripts/report.js
@@ -0,0 +1,24 @@
+async function report() {
+    let page = document.getElementById("page").value;
+    let reason = document.getElementById("reason").value;
+
+    page = HtmlSanitizer.SanitizeHtml(page);
+    reason = HtmlSanitizer.SanitizeHtml(reason);
+    
+    console.log(page);
+    console.log(reason);
+
+    let str = `{pg:"${page}",rs:"${reason}"}`;
+
+    let options = {
+        method: "POST",
+        body: JSON.stringify(str)
+    }
+    await fetch("https://api.xefili.dev/r", options)
+}
+
+document.getElementById("reportform").addEventListener("submit", (event) => {
+    event.preventDefault();
+})
+
+window.report = report;
